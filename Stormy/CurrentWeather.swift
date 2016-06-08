e857eecdc5bf044830eaff3c8d8458e6b17be419
avijeet <a@avijeets.com>
2016-06-08 09:17:35 -0400

    Initial Commit

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Avijeet Sachdev on 6/8/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: UIImage
+}
---
+++ Stormy/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Pasan Premaratne on 4/9/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
